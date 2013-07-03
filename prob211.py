1c820d9c2e0463ade785cdb2ccf4f6a262e6b93a
mercurium <jerrychen434@gmail.com>
2013-07-02 17:01:05 -0700

    finished problem 211! :D

--- prob211.py
+++ prob211.py
@@ -1,19 +1,29 @@
 from math import *
 import time
 start = time.time()
+import sys
 
-lst = [1]*100000
-for i in range(2,len(lst)):
-  for j in range(i,len(lst),i):
-    lst[j] += i**2
+size = 10**5
+if len(sys.argv) > 1:
+	size = int(sys.argv[1])
+
+lst = [1]*size
+for i in xrange(2,len(lst)):
+	if i%1024 == 0:
+		print i
+	a = i**2
+	for j in xrange(i,len(lst),i):
+		lst[j] += a
 
 count = 0
-for i in range(1,len(lst)):
-  if int(sqrt(lst[i])+.1)**2 == lst[i]:
-    print i, lst[i]
-    count +=1
+sumz = 0
+for i in xrange(1,len(lst)):
+	if int(sqrt(lst[i])+.1)**2 == lst[i]:
+		print i, lst[i]
+		count +=1
+		sumz +=i
 print count
-
+print sumz
 print "time elapsed = " + str(time.time()-start)
 
 
