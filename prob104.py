c850c34592bf6356471bf3e2706a87afcac2bc65
mercurium <jerrychen434@gmail.com>
2013-07-21 00:41:19 -0700

    solved problem 104 :D

--- prob104.py
+++ prob104.py
@@ -3,53 +3,54 @@ import math
 import time
 start = time.time()
 
-def rep_sq(n, powz, mod):
+def approx_pow(n,powz,dig):
         if powz == 0: return 1
-        if powz == 1: return n % mod 
-    
-      
+        if powz == 1: return truncate(n,dig)
+
         val = int(math.log(powz,2))
-        lst = [1,n] + [0] * val 
-        for i in range(2,len(lst)):
-                lst[i] = lst[i-1]**2 % mod 
-      
+        lst = [1,n] + [0] * val
+        for i in xrange(2,len(lst)):
+                lst[i] = truncate(lst[i-1]**2,dig)
+
         pows = [0] * (val+1)
         power = powz
-        for i in range(0,len(pows)):
-                pows[i] = power % 2 
-                power = power //2 
-        product = 1 
-        for i in range(0,len(pows)):
+        for i in xrange(0,len(pows)):
+                pows[i] = power % 2
+                power = power //2
+        product = 1
+        for i in xrange(0,len(pows)):
                 if pows[i] == 1:
-                        product = product * lst[i+1] % mod 
+                        product = truncate(product * lst[i+1],dig)
         return product
 
-vals = (1,1,2)
+def truncate(num, dig):
+        num_len = int(math.log(num,10))
+        if num_len < dig:
+                return num
+        return num/10**(num_len -dig)
 
+
+mod = 10**9
+vals = (1,1)
 a = (5**.5+1)/2
 
-def find_R(n):
-	return 0
- 
+def find_L(n):
+	approx_head = int(truncate(approx_pow(a,n,50)/(5**.5),8))
+	val = string.join(sorted(str(approx_head)),'')
+	return  (val  == '123456789', val, approx_head)
+
+for i in xrange(3,5* 10**5):
+	vals = (vals[1],(vals[0]+vals[1])%mod)
+	if string.join(sorted(str(vals[1])), '') == "123456789":
+		L_works, value,junk = find_L(i)
+		if L_works: 
+			print i
+			break
+		else:
+			print "so close, but on:", i, value, junk
+
 print "Time Taken: ", time.time()-start
 
 
 """
-vals = (1,1,2)
-count = 4
-for i in xrange(500000):
-  vals = (vals[1],vals[2],vals[1]+vals[2])
-  val = (string.join(sorted(str(vals[2])[:9]), '') == '123456789', string.join(  sorted(  str(vals[2])[-9:]  ), '') == '123456789')
-  if val == (True,True):
-    print i+count
-    break
-  if val[0] == True:
-    print "Front of ", i+count, "is pan"
-  if val[1] == True:
-    print "Back of ", i+count, "is pan"
-    
-print "Time Taken: ", time.time()-start
-
-
-okay, so using that one technique I learned about, this could be feasible... o.o... now to figure out how to deal with it :D;;;
 """
