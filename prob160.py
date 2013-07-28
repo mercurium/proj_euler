8a3d4017b0a87cab1052e7bb7527d3f8e75c8399
mercurium <jerrychen434@gmail.com>
2013-07-28 14:41:50 -0700

    updated 160 a bit again...

--- prob160.py
+++ prob160.py
@@ -9,12 +9,12 @@ MOD = 10**MOD_POW
 SIZE = 10**POWER
 
 #second parameter is for if we want to ignore the multiples of 10. 1=ignore,0 = don't ignore.
-def fa(n, ignore_10): 
+def fa(n, ignore_10=0): 
 	if n == 0 or n == 1:
 		return 1
 	prod = 1
 	two_count = 0
-	for i in xrange(2,n):
+	for i in xrange(2,n+1):
 		a = i
 		if i%10==0 and ignore_10:
 			continue
@@ -32,10 +32,10 @@ def fa(n, ignore_10):
 val = fa(MOD,1)
 print val
 
-POWER_necs = sum(10**x for x in xrange(POWER-MOD_POW+1))
-print POWER_necs
-vals_after = fa(MOD/10,0)
-print (pow(val,POWER_necs,MOD) * vals_after)%MOD 
+POWER_NECS = sum(10**x for x in xrange(POWER-MOD_POW+1))
+print POWER_NECS
+vals_after = fa(MOD/10)
+print (pow(val,POWER_NECS,MOD) * vals_after)%MOD 
 print "Time Taken:", time.time() -start
 
 
