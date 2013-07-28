8692c977e43757e10a7bdfd19ad0b604df99257d
mercurium <jerrychen434@gmail.com>
2013-07-28 10:02:29 -0700

    updated problem160, not much progress...

--- prob160.py
+++ prob160.py
@@ -2,26 +2,45 @@ import time
 start = time.time()
 from math import factorial as fact
 
-def fa(n):
-  if n == 0 or n == 1:
-    return 1
-  prod = 1
-  for i in xrange(2,n+1):
-    prod *= i
-    while prod % 10 == 0:
-      prod /= 10
-    prod = prod % (10**5)
-  return prod
 
-val = fa(10**5)
-#last 4 digits of factorial(10**5) are 2496
+POWER = 12 
+MOD_POW = 5
+MOD = 10**MOD_POW
+SIZE = 10**POWER
+
+#second parameter is for if we want to ignore the multiples of 10. 1=ignore,0 = don't ignore.
+def fa(n, ignore_10): 
+	if n == 0 or n == 1:
+		return 1
+	prod = 1
+	two_count = 0
+	for i in xrange(2,n):
+		a = i
+		if i%10==0 and ignore_10:
+			continue
+		while a%2 == 0:
+			a /= 2
+			two_count += 1
+		while a%5 == 0:
+			a /= 5
+			two_count -= 1
+		prod = (prod * a)% MOD
+	prod = (pow(2,two_count,MOD) * prod)% MOD
+	return prod
+
+
+val = fa(MOD,1)
 print val
-print pow(val,10**7,10**10)
+
+POWER_necs = sum(10**x for x in xrange(POWER-MOD_POW+1))
+print POWER_necs
+vals_after = fa(MOD/10,0)
+print (pow(val,POWER_necs,MOD) * vals_after)%MOD 
 print "Time Taken:", time.time() -start
 
 
 
-#ahh, dammit >.>;; The reason for the incongrinuity... is because if we have like... 12300000000, it becomes 123, not another random number we cancel out... T___T
-
-
-#okay, so I'm going to test first ignoring all numbers that are divisible by 10, then move up to ignoring those divisible by 100, then 1000, etc.
+"""
+ahh, dammit >.>;; The reason for the incongrinuity... is because if we have like... 12300000000, it becomes 123, not another random number we cancel out... T___T
+okay, so I'm going to test first ignoring all numbers that are divisible by 10, then move up to ignoring those divisible by 100, then 1000, etc.
+"""
