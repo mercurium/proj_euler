95c65778a94024b64c7b4cb68957acd8453b8049
mercurium <jerrychen434@gmail.com>
2013-08-04 14:34:04 -0700

    updating prob329 again...

--- prob329.py
+++ prob329.py
@@ -2,35 +2,37 @@ import time
 start = time.time()
 from bitarray import bitarray
 
+SIZE = 500
+
 def gcd(a,b):
         if a == 0:
                 return b
         return gcd(b%a,a)
 
-primes = bitarray('11'+'0' *499)
+primes = bitarray('11'+'0' *(SIZE-1))
 for i in xrange(2,501):
         if primes[i] == 0:
-                for j in xrange(2*i,501,i):
+                for j in xrange(2*i,(SIZE+1),i):
                         primes[j] = 1
 
 
 seq = bitarray('000011000100101')
-squares = [0] + [1] * 500
-new = [0] * 501
-div = 3**15 * 500 * 2**15
+squares = [0] + [1] * SIZE 
+new = [0] * (SIZE+1)
+div = 3**15 * SIZE * 2**15
 
 
 for i in xrange(0,15):
         new[1] = squares[2]
-        new[500] = squares[499]
-        for j in xrange(2,500):
+        new[SIZE] = squares[SIZE-1]
+        for j in xrange(2,SIZE):
                 new[j] = squares[j-1] + squares[j+1]
 
-        for j in xrange(1,501):
+        for j in xrange(1,SIZE+1):
                 if seq[i] == primes[j]:
                         new[j] *= 2
         squares = new[:]
-        new = [0]*501
+        new = [0]*(SIZE+1)
 
 a = sum(squares)
 shared = gcd(a,div)
