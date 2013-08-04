2289114ccdd83fce6a362813a84fb68fc8a17dcc
mercurium <jerrychen434@gmail.com>
2013-08-04 12:49:55 -0700

    updated problem 43

--- prob43.py
+++ prob43.py
@@ -1,10 +1,8 @@
-from primes import *
 import time
-import itertools
-import string
-perm = itertools.permutations
-
 start = time.time()
+from itertools import permutations as perm
+import string
+
 lst = perm(['1','2','3','4','5','6','7','8','9','0'],10)
 items = set()
 #we check in the order of numbers with dig 456 divisible by 5 first since it eliminates 80% of the numbers. We can also check it based on the last digit and without wasting time converting it to an int.
@@ -16,17 +14,6 @@ for item in lst:
   n = string.join(item, '') #1-3,2-5,3-6,4-7,5-8
   if n[5] in '05' and n[3] in '02468' and int(n[2:5])%3 == 0 and int(n[4:7])%7 ==0 and int(n[5:8])%11==0 and int(n[6:9])%13 == 0 and int(n[7:10])%17 == 0:
     items.add(int(n))
-print sum(list(items))
+print sum(items)
 print items
-
-
-
-
-print "Time taken: " + str(time.time() -start)
-
-
-
-
-
-
-
+print "Time taken:",time.time() -start
