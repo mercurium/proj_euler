decfce4561437c8cbfdf77bb93ab3c836292c4f8
mercurium <jerrychen434@gmail.com>
2013-08-05 09:27:52 -0700

    updated 233 a bit... =/ still not solved.

--- prob233.py
+++ prob233.py
@@ -7,7 +7,6 @@ def gcd(a,b):
         return gcd(b%a,a)
 
 SIZE = 10**5
-SIZE_SQ = SIZE**2
 
 iter_count = 0 
 actual_count = 0 
@@ -15,32 +14,33 @@ actual_count = 0
 vals = dict()
 
 for m in xrange(1,SIZE):
-	if 2*m**2 > SIZE:
+	if m**2 > SIZE:
 		break
-        for n in xrange(1,m+1):
+        for n in xrange(1,m+1):	
+		item = m**2+n**2
+		if item > SIZE:
+			break
 		for k in xrange(1,SIZE):
 			iter_count+=1
-			N = 2*k**2*(m**2+n**2)
-			if N > 2*SIZE:
+			N = k*(item)
+			if N > SIZE:
 				break
-			a,b = 2*k**2*(m**2-n**2),4*m*n*k**2
+			a,b = k*(m**2-n**2),2*m*n*k
 			a,b = min(a,b),max(a,b)
 
 			if N in vals:
 				vals[N].add((a,b))
 			else:
 				vals[N] = set([(a,b)])
-                	actual_count +=1 
 
 
-print "I iterated over:", iter_count
-print "But we only counted for:", actual_count
+print "I iterated over:", iter_count, "numbers"
 
-print "Count for 10000 is:", vals[10000]
+print "Count for 10000 is:", len(vals[10000])*8-4
 
 sumz = 0
 for key in vals.keys():
-	if vals[key] == 140:
+	if len(vals[key]) == 53:
 		sumz += key
 		print key
 
