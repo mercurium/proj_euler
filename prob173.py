aea4ef406d380654271797c655458cbc4e351ca2
mercurium <jerrychen434@gmail.com>
2013-08-09 22:39:45 -0700

    updated problem 173

--- prob173.py
+++ prob173.py
@@ -1,23 +1,22 @@
-#import string
 import math
-
+import time
+start = time.time()
 n = 10**6
 
-#for i in range(3,n/4+2):
- # for j in range(1,i):
-  #  if i**2-j**2 <= n and (i -j)%2 == 0 :
-   #   count = count +1
-    #  print i,j, (i**2-j**2)/4
-
-#print count
 
 
 count = 0
 i = 2
 while (n - i**2)/(2*i) > 0:
   count = count + (n - i**2)/(2*i)
-  print (n - i**2)/(2*i)
+  #print (n - i**2)/(2*i)
   i = i +2
 
 
 print count
+print "Time Taken:", time.time() - start
+"""
+22:38 ~/Desktop/python_projects/proj_euler $ python prob173.py 
+1572729
+Time Taken: 0.000257968902588
+"""
