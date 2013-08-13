e387df0f41add739d19d07729137ef822117bfa4
mercurium <jerrychen434@gmail.com>
2013-08-12 17:48:20 -0700

    updated problem 417

--- prob417.py
+++ prob417.py
@@ -1,9 +1,11 @@
 import time
 start = time.time()
-from primes import *
-from bitarray import bitarray
+import sys
 
-SIZE = 10**3
+if len(sys.argv) > 1:
+	SIZE = int(sys.argv[1])
+else:
+	SIZE = 10**3
 
 def gcd(a,b):
 	if a == 0:
@@ -22,8 +24,18 @@ def get_plst(size):  #returns a list of numbers, each of which are a prime facto
 			for j in xrange(i**2,len(lst),2*i):
 				lst[j] = i
 	return lst
-
 plst = get_plst(SIZE)
+
+def get_totient(size):
+	lst = range(size+1)
+	lst[0] = 1
+	for i in xrange(2,len(lst)):
+		if lst[i] == i:
+			for j in xrange(i,len(lst),i):
+				lst[j] = (lst[j] * (i-1))/i 
+	return lst
+totient = get_totient(SIZE)
+
 
 def pfactor(n): #returns the entire list of prime factors of n
 	factors = []
@@ -41,41 +53,46 @@ def get_divisors(n): # returns all divisors of n.
 			new_set.add(f*d)
 		for i in new_set:
 			divisors.add(i)
-
 	return sorted(list(divisors))
 
-
-
 def rep_dig(n):
-	powz = 1
-	while pow(10,powz,n) != 1:
-		powz +=1
-	return powz
-	return val
+	potential_pows = get_divisors(totient[n])
+	for powz in potential_pows:
+		if pow(10,powz,n) == 1:
+			return powz
+	return -1
 
-	
+def old_rep_dig(n):
+	powz = 1	
+	while pow(10,powz,n) != 1:
+		powz +=1	
+	return powz
 
 values = [0] * SIZE			
-for i in xrange(3,SIZE,2):
-	if i%1024 == 0:
+values[3] = 1
+for i in xrange(5,SIZE,2):
+	if i %1024 == 1:
 		print i
 	if i %5 == 0:
 		values[i] = values[i/5]
 		continue
-	if i == pfactor[i]:  #all primes have that 10^p = 1 mod p...
-		values[i] = i-1
-		continue
 	values[i] = rep_dig(i)
+	
 
 for i in xrange(2,SIZE,2):
 	values[i] = values[i/2]
 
+#print values
 print sum(values)
 print "time taken: " +str(time.time()-start)	
 
 
 """
-For comparison sake, 139866
+For comparison sake, 94288
 is the right answer for size 10^3
+
+17:43 ~/Desktop/python_projects/proj_euler $ python prob417.py 1000000
+55535191115
+time taken: 22.1281189919
 
 """
