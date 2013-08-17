9d551f417a8d17e385e56720a7b83608462b457c
mercurium <jerrychen434@gmail.com>
2013-08-16 18:06:57 -0700

    refactored problem 44

--- prob44.py
+++ prob44.py
@@ -3,22 +3,25 @@ import math
 import time
 start = time.time()
 
-items = set()
-for i in range(1,50000):
-  items.add(i*(3*i-1)/2)
-print "Time Taken: " + str(time.time()-start)
+pentas = set()
+for i in xrange(1,50000):
+	pentas.add(i*(3*i-1)/2)
+
+penta_lst = [ i * (3*i-1)/2 for i in xrange(10000)]
 
 done = 0
-for i in range(2,10000):
-  for j in range(1,i):
-    a = i*(3*i-1)/2
-    b = j*(3*j-1)/2
-    if a+b in items and a-b in items:
-      print a-b, a, b, a+b
-      print i,j
-      done = 1
-      break
-  if done:
-    break
+for i in xrange(2,10000):
+	for j in xrange(1,i):
+		a = penta_lst[i] 
+		b = penta_lst[j] 
+		if a+b in pentas and a-b in pentas:
+			print a-b, a, b, a+b
+			print i,j
+			done = 1
+			break
+	if done:
+		break
 
-print "Time Taken: " + str(time.time()-start)
+
+print "The answer is:", a-b
+print "Time Taken:", time.time()-start
