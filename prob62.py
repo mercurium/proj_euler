dda30f43b89d08f0bb460e77a5dce68f159509bb
Jerry Chen <jerrychen434@gmail.com>
2013-08-22 13:21:30 -0700

    refactored 62,64, and touched on 65 a bit, not enough though...

--- prob62.py
+++ prob62.py
@@ -1,15 +1,12 @@
 import time
 import string
-start = time.time()
+START = time.time()
 
-d = {}
-
-key = ''
-for i in range(0,10000):
-  m = string.join(sorted(str(i**3) ),'' )
-  try: d[m] += [i]
-  except: d[m] = [i]
-  if len(d[m]) == 5:
-    print min(d[m])**3, min(d[m])
-    break
-print "Time Taken: " + str(time.time() -start)
+cubic_nums_sorted = {}
+for i in xrange(0,10000):
+	m = string.join(sorted(str(i**3) ),'' )
+	cubic_nums_sorted[m] = (cubic_nums_sorted[m] + [i]) if m in cubic_nums_sorted else [i]
+	if len(cubic_nums_sorted[m]) == 5:
+		print min(cubic_nums_sorted[m])**3, min(cubic_nums_sorted[m])
+		break
+print "Time Taken:", time.time()-START
--- prob64.py
+++ prob64.py
@@ -2,44 +2,42 @@ from primes import *
 from math import sqrt
 import math
 import time
-start = time.time()
+START = time.time()
 
-#the way to get the next item in the list is such:
-# A' = a*B -A
-# B' = (d-A'*A')/B
-# a' = int((sqrt(d)+A')/B')
-def transform(A,B,a,d):
-  A = a*B-A
-  B = (d-A*A)/B
-  a = int((sqrt(d)+A)/B)
-  return A,B,a
+"""
+the way to get the next item in the list is such:
+ A' = a*B -A
+ B' = (d-A'*A')/B
+ a' = int((sqrt(d)+A')/B')
+"""
+def get_next_iter(A,B,a,d):
+	A = a*B-A
+	B = (d-A*A)/B
+	a = int((sqrt(d)+A)/B)
+	return A,B,a
 
 count = 0
-for d in range(2,10000): #iterate from 2 to 10000
-  if int(sqrt(d+.2))**2 != d:
-    grub =transform(0,1,int(sqrt(d)),d)
-    lst_A =[grub[0]]
-    lst_B =[grub[1]]
-    lst_a =[grub[2]] #this updates the list
-    
-    grub =transform(lst_A[0],lst_B[0],lst_a[0],d)
-    lst_A += [grub[0]]
-    lst_B += [grub[1]]
-    lst_a += [grub[2]] #this updates the list again
-    i=1
-    while lst_A[-1]!=lst_A[0] or lst_B[-1]!=lst_B[0] or lst_a[-1] != lst_a[0]: #while it hasn't repeated yet...
-      grub =transform(lst_A[i],lst_B[i],lst_a[i],d)
-      lst_A += [grub[0]]
-      lst_B += [grub[1]]
-      lst_a += [grub[2]]
-      i+=1 #our fancy loop counter
+for d in xrange(2,10000): #iterate from 2 to 10000
+	if int(sqrt(d+.2))**2 != d:
+		next_iter =get_next_iter(0,1,int(sqrt(d)),d)
+		lst_A =[next_iter[0]]
+		lst_B =[next_iter[1]]
+		lst_a =[next_iter[2]] #this updates the list
+		
+		next_iter =get_next_iter(lst_A[0],lst_B[0],lst_a[0],d)
+		lst_A += [next_iter[0]]
+		lst_B += [next_iter[1]]
+		lst_a += [next_iter[2]] #this updates the list again
+		i=1
+		while lst_A[-1]!=lst_A[0] or lst_B[-1]!=lst_B[0] or lst_a[-1] != lst_a[0]: #while it hasn't repeated yet...
+			next_iter =get_next_iter(lst_A[i],lst_B[i],lst_a[i],d)
+			lst_A += [next_iter[0]]
+			lst_B += [next_iter[1]]
+			lst_a += [next_iter[2]]
+			i+=1 #our fancy loop counter
 
 
-    if len(lst_a)%2 == 0: #if it has an odd cycle, add to count
-      count += 1
+		if len(lst_a)%2 == 0: #if it has an odd cycle, add to count
+			count += 1
 print count
-
-
-print "Time taken: " + str(time.time()-start)
-
-
+print "Time Taken:", time.time()-START
