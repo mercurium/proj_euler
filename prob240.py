a33a1a367770d1eb6c5e75a586a2be8b1de3c177
mercurium <jerrychen434@gmail.com>
2014-02-17 00:51:15 -0800

    solved problem 240!

--- prob240.py
+++ prob240.py
@@ -1,20 +1,44 @@
 import time
-start = time.time()
+START = time.time()
+from math import factorial as fa
 
-size = 6
-lst1 = [1]*size + [0]*(100-size)
-lst2 = [0]*100
+numDie = 20
+maxDieNum = 12
+total = 70
+topDice = 10
+
+def countDie(minNum, dieNum, sumz,prod, numInRow):
+    if minNum > maxDieNum or sumz > total:
+        return 0
+    if dieNum == numDie:
+        if sumz == total:
+            return fa(numDie)/prod /fa(numInRow)
+        else:
+            return 0
+    if dieNum >= numDie-topDice:
+        a = countDie(minNum, dieNum+1,sumz+minNum,prod,numInRow+1)
+    else:
+        a = countDie(minNum, dieNum+1,sumz,prod, numInRow+1)
+    b = countDie(minNum+1,dieNum,sumz, prod*fa(numInRow),0)
+    return a+b
 
 
+print countDie(1,0,0,1,0)
 
-for k in range(1,3): #counting distribution of pete
-  for i in range(1,size+1):
-    for j in range(0,len(lst1)-i):
-      lst2[i+j] += lst1[j]
-  lst1 = lst2[:]
-  lst2 = [0]*(100)
-print lst1[14]
+print "Time Taken:", time.time() - START
 
 
+"""
+Congratulations, the answer you gave to problem 240 is correct.
 
-print "Time Taken: " + str(time.time() -start)
+You are the 1352nd person to have solved this problem.
+
+Return to Problems page.
+
+00:34 ~/Desktop/python_projects/proj_euler $ python prob240.py 
+7448717393364181966
+Time Taken: 88.1096231937 (using arrays and computing sum at end)
+Time Taken: 71.6896748543 (computing sum as running total, saves space)
+
+
+"""
