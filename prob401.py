cd0bed92021535cdfc03d8539ab69d2cb21d2580
Jerry Chen <jerrychen434@gmail.com>
2014-06-07 07:22:56 -0700

    Solved problem 401

--- prob401.py
+++ prob401.py
@@ -1,61 +1,52 @@
-#NOTE TODO need to solve it
 import time
-start = time.time()
+START = time.time()
 
-mult = 10**15
-size = 10**9
-count = 0
-num1,num2 = 1,1
-set_to_check = set()
-set_to_check2 = set()
+SIZE = 10**15 
+MOD  = 10**9 
+count = 0  #Our accumulator variable
 
-for i in xrange(1,10**6):
-  if mult/(i* size) != mult/(size*i+num1):
-    print i
-    count +=1
-    set_to_check.add(i)
+#Formula here: http://en.wikipedia.org/wiki/Square_pyramidal_number
+def sumSq(n): 
+    return n*(n+1) *(2*n+1)/6
 
-print '\n', count
-print "Time Taken: ", time.time()-start
+for d in xrange(1,10**7*2+1):
+    n = SIZE/d
+    count += sumSq(n)
+print count % 10**9
+print "Time Taken:", time.time()-START
 
 
-for i in xrange(1,10**6):
-  if mult/(i* size) != mult/(size*i+num2):
-    print i
-    count +=1
-    set_to_check2.add(i)
+for d in xrange(1, 10**7*5):
+    count += (SIZE/d - 10**7*2) * d**2
 
-print '\n', count
-print "Time Taken: ", time.time()-start
-
+print count % 10**9
+print "Time Taken:", time.time()-START
 
 
 """
-m = multiplicative factor,  10^15
-s = size that we mod by, 10^9
-n = 10^6, largest int n where n * s <= m. 10^6 for set values
-a = adjusted n when we have adjusted s.
-R = number that we multiply s by, when we get our position/10^9
+Congratulations, the answer you gave to problem 401 is correct.
+
+You are the 1038th person to have solved this problem.
+
+For this problem, there are (at least) two ways to count the sum of the squares of the divisors from 1 to N. 
+
+The first way is to compute N/d, for every d up to N, and multiply it by d^2 for the divisors squared sum
+The second way is to see how many squares occur once, occur twice, and etc. We then have from that the numbers from 1 to N/d occur at least d times.
+
+Unfortunately, using either of these methods individually is slow (10^15 divisions anyone?), but surprisingly, combining the two allows us to reduce the problem size to something only a square root of the size. Since the two approaches come in from opposite sides (one is high to low, the other is low to high), we can use the one that is more efficient at each chunk, and thus do approximately 7 * 10^7 operations instead of 10^15.
 
 
-ALL INTS
+First run:
+~/proj_euler $python prob401.py
+Time Taken: 7.84915804863 #first half
+281632621
+Time Taken: 47.5886650085 #total runtime
 
-variables to find:
-n, a 
-
-
-Given:
-R, s, k
-
-m >= R*s*n
-m >= (R*s+k)(n-a)
-
-We want to find when a = 0, given R,s, k. Should be easy...
-
-
-
-
-
+Upon restructuring the limits, I reduced it to:
+Time Taken: 34.7627840042
+And after removing the %MOD, and removing the function call, I get
+Time Taken: 31.6589548588
+but it's not worth keeping since it makes the code less readable and not that much faster
 
 
 
