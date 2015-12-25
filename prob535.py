d0c7b9a048d88d024a3655c707d201ee90a395c8
mercurium <jerrychen434@gmail.com>
2015-12-25 02:14:45 -0800

    solved problem 535

--- prob535.py
+++ prob535.py
@@ -1,96 +1,72 @@
-import time, math
+import time, math, sys
+sys.setrecursionlimit(500)
+
+SIZE  = 10**18
 START = time.time()
-SIZE  = 10**3
 
-arr            = [1,1]
-nextCircledInt = 2
-
-for arrIndex in xrange(1,SIZE):
-  if len(arr) > SIZE:
-    break
-  nextNum = arr[arrIndex]
-
-  # Add on the next math.sqrt(nextNum) on at a time, and increment the counter as such
-  arr.extend(xrange(nextCircledInt, nextCircledInt+int(math.sqrt(nextNum))))
-  nextCircledInt += int(math.sqrt(nextNum))
-
-  # Append the number that we just incremented by
-  arr.append(nextNum)
-
-arr = arr[:SIZE]
-
-countArr = [0] * (max(arr) + 1)
-for i in xrange(len(arr)):
-  countArr[arr[i]] += 1
-
-print sum(arr[:SIZE])
-print "Time taken:", time.time() - START
-
-# This chunk is the part for finding the answer once I find the frequency of answers
-nextVal = 1
-for countVal in xrange(max(countArr), 1, -1):
-  if (countVal-1) in countArr:
-    nextVal = max(countArr.index(countVal-1)-1, 1)
-  print str(countVal) + ":", nextVal, '\t'
-print "1:", len(countArr) -1, '\t'
-# This chunk is the part for finding the answer once I find the frequency of answers
-
-"""
-Sum of floored square roots from 1 to n, where m = int(sqrt(n)) is:
-  1 * (2 * 1 + 1) + 2 * (2 * 2 + 1) + ... + m * (2*m +1) + (m+1) * (leftovers)
-  = sum(i=1 to m)( 2m^2 + m) + leftovers
-  -> 2 * (m * (m+1) * (2*m+1))/6 + m(m+1)/2 + leftovers
-  -> m * (m+1) * (2m+1)/3 + m(m+1)/2 + leftovers
-"""
+# Technically unused, but it's useful for computing an approximation for how high the next number in the recursion would be.
 def getNum(n):
   sqrtN  = int(math.sqrt(n))
   return (sqrtN * (sqrtN-1) * (sqrtN * 2 - 1))/ 3 \
          + (sqrtN * (sqrtN - 1 )) / 2 \
          + max(0,(n + 1 - sqrtN**2)) * sqrtN
+gn = getNum
 
-def successFunc(target, num):
-  if getNum(num) > target:
-    return 1
-  elif getNum(num) + int(math.sqrt(num+1)) < target:
-    return -1
-  return 0
-
-def binSearch(lowerBound, upperBound, target):
+# Super simple binary search function
+def binSearch(func, lowerBound, upperBound, target):
   mid = (lowerBound + upperBound) / 2
-  result = successFunc(target, mid)
-  if result == 0:
-    return mid
-  elif result < 0:
-    return binSearch((lowerBound + upperBound)/2, upperBound, target)
+  result = func(mid)
+  if result > target:
+    return binSearch(func, lowerBound, (lowerBound + upperBound)/2, target)
+  elif func(mid+1) < target:
+    return binSearch(func, (lowerBound + upperBound)/2, upperBound, target)
   else:
-    return binSearch(lowerBound, (lowerBound + upperBound)/2, target)
+    return mid
 
-def getAnsFunc(n):
-  print "going two levels deeper on:", n
-  getNumResult = getNum(n)
-  oneLvlLower = binSearch(getAnsFunc2, 0, n, n)
-  print "The getNum result for", n, "is:", getNumResult, n, oneLvlLower
-  return getNumResult + oneLvlLower
+def createApproxAnswerArray(size):
+  arr = [size]
+  for i in xrange(20):
+    m = arr[-1]
+    if m == 1:
+      break
+    arr.append(binSearch(lambda x: x + gn(x), 0, m, m))
+  
+  arr[0] = gn(arr[1])
+  arr.append(1)
+  arr = arr[::-1]
+  return arr
 
-def getAnsFunc2(n):
-  print "going one level deeper on:", n
-  getNumResult = getNum(n)
-  oneLvlLower = binSearch(getNum, 0, n, n)
-  print "The getNum2 result for", n, "is:", getNumResult, n, oneLvlLower
-  return getNumResult + oneLvlLower
+def fixUpAnswerArray(arr, size):
+  for k in xrange(10):
+    for j in xrange(len(arr)):
+      for i in xrange(1,len(arr)-1):
+        getPastNumSum = lambda x: sum([gn(elem) for elem in arr[:i]]) + gn(x)
+        arr[i]        = binSearch(getPastNumSum, 0, arr[i+1], arr[i+1])
 
-target     = 10**2
-upperBound = int((target * 3/2.) ** (2/3.) * 1.15)
-lowerBound = int((target * 3/2.) ** (2/3.))
-mid        = binSearch(lowerBound, upperBound, target)
-print mid
+        while arr[i] > sum([gn(x) for x in arr[:i-1]]) + gn(arr[i-1]+1):
+          arr[i-1] +=1
+        while arr[i] < sum([gn(x) for x in arr[:i]]):
+          arr[i-1] -=1
+    
+    arr[-1] = size - sum(arr[:-1])
+  return arr
 
-print "Time taken:", time.time() - START
+def mainFunc(size):
+  arr = createApproxAnswerArray(size)
+  return fixUpAnswerArray(arr, size)
+
+answer = sum([ x*(x+1)/2 for x in mainFunc(SIZE)])
+print answer % 10**9
+print "Time Taken:", time.time() - START
+
 
 """
-
-
 We can sort of use "sum(int(math.sqrt(x)) for x in xrange(1,12926)) -> 973269 (actual # is 986231)"
+
+Answer: 611778217
+Time Taken: 0.133980989456
+
+I feel like I sort of cheated... I mean, I got the right answer, and this code is solely my own, but it doesn't necessarily give the right answer for some sizes... :x
 
 
 """
