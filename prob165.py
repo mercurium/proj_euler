a5d01eea52302a58b8125070c9cadc2e2aeb17b9
Jerry Chen <jchen@palantir.com>
2016-06-01 21:05:56 -0400

    solved prob254, cleaned up prob165... though it's still wrong

--- prob165.py
+++ prob165.py
@@ -1,71 +1,104 @@
 #NOTE TODO need to solve it
 import time
-START = time.time()
+from fractions import Fraction
 
-def sign(n):
-	if n >0:
-		return 1
-	if n == 0:
-		return 0
-	return -1
+START = time.time()
+SIZE  = 5000
 
 class Point():
-	def __init__(self, x,y):
-		self.x = x
-		self.y = y
+  def __init__(self, x,y):
+    self.x = x
+    self.y = y
+
+  def toString(self):
+    return "(" + str(self.x) +"," + str(self.y)+ ")"
 
 class Line():
-	def __init__(self, start,end):
-		if start.x > end.y:
-			start,end = end,start
-		self.start = start
-		self.end = end
-		if start.x != end.x:
-			self.slope = (end.y - start.y)*1.0/(end.x - start.x)
-		else:
-			self.slope = 10**6
-		self.yInter = start.y - self.slope * start.x
+  def __init__(self, start,end):
+    if start.x > end.x:
+      start,end = end,start
 
-	def isIntersecting(self, l):
-		if self.slope == l.slope:
-			return False
-		if self.slope == 10**6:
-			if l.slope == 10**6:
-				return False
-			if l.start.x < self.x and l.end.x > self.start.x and 
+    self.start = start
+    self.end   = end
+
+    if start.x != end.x:
+      self.slope = Fraction((end.y - start.y),(end.x - start.x))
+    else:
+      self.slope = 10**6
+    self.yInter = start.y - self.slope * start.x
+
+  def isIntersecting(self, l):
+    if self.slope == l.slope:
+      return False
+
+    if self.end.x < l.start.x \
+        or self.start.x > l.end.x \
+        or max(self.end.y,self.start.y) < min(l.start.y,l.end.y) \
+        or min(self.end.y,self.start.y) > max(l.start.y,l.end.y):
+          return False
+
+    if self.slope == 10**6:
+      intersect = l.yInter + self.start.x * l.slope
+      if intersect < max(self.start.y, self.end.y) and intersect > min(self.start.y, self.end.y):
+        intersectionPoints.add((self.start.x, intersect))
+        return True
+      return False
+
+    if l.slope == 10**6:
+      intersect = self.yInter + l.start.x * self.slope
+      if intersect < max(l.start.y, l.end.y) and intersect > min(l.start.y, l.end.y):
+        intersectionPoints.add((l.start.x, intersect))
+        return True
+      return False
+
+    if self.slope == 0:
+      intersect = (self.start.y - l.yInter) / l.slope
+      if intersect > self.start.x and intersect < self.end.x:
+        intersectionPoints.add((intersect, self.start.y))
+        return True
+      return False
+
+    if l.slope == 0:
+      intersect = (l.start.y - self.yInter) / self.slope
+      if intersect > l.start.x and intersect < l.end.x:
+        intersectionPoints.add((intersect, l.start.y))
+        return True
+      return False
 
 
-		a = sign(l.end.y - self.slope * l.end.x - self.yInter)
-		b = sign(l.start.y - self.slope * l.start.x - self.yInter)
-		c = sign(self.end.y - l.slope * self.end.x - l.yInter)
-		d = sign(self.start.y - l.slope * self.start.x - l.yInter)
-		if a == 0 or b== 0 or c == 0 or d == 0:
-			return False
-		if a != b and c != d:
-			return True
-		return False
+    intersect  = (self.yInter - l.yInter) / (l.slope - self.slope)
+    intersectY = self.yInter + intersect * self.slope
 
-n = 290797
-mod = 50515093
+    if intersect < self.end.x and \
+        intersect > self.start.x and \
+        intersect < l.end.x and \
+        intersect > l.start.x:
+      intersectionPoints.add((intersect, intersectY))
+      return True
+    return False
+
+  def toString(self):
+    return "Line(" +self.start.toString() + "," + self.end.toString() + ")"
+
+
+n     = 290797
+mod   = 50515093
 lines = []
-for i in range(5000):
-	n = n*n%mod
-	xS = n % 500
-	n = n*n%mod
-	yS = n % 500
-	n = n*n%mod
-	xE = n % 500
-	n = n*n%mod
-	yE = n% 500
-	lines.append(Line(Point(xS,yS),Point(xE,yE)))
+for i in range(SIZE):
+  vals = [pow(n,2,mod) % 500, pow(n,4,mod) % 500, pow(n,8,mod) % 500, pow(n,16,mod) % 500]
+  n    = pow(n,16,mod)
+  lines.append(Line(Point(vals[0], vals[1]), Point(vals[2], vals[3])))
 
-count = 0
-for i in range(5000):
-	for j in range(i+1,5000):
-		if lines[i].isIntersecting(lines[j]):
-			count +=1
-	if i% 100 == 0:
-		print i, count
+print lines[0].toString()
+count  = 0
+intersectionPoints = set()
+for i in range(SIZE):
+  for j in range(i+1,SIZE):
+    if lines[i].isIntersecting(lines[j]):
+      count +=1
+  if i% 100 == 0:
+    print i, count
+print len(intersectionPoints)
 
 print count
 print "Time Taken:", time.time() - START
