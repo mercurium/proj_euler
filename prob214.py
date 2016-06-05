580b6896f81e0cb33af51e2049d01657ac504f71
Jerry Chen <jchen@palantir.com>
2016-06-05 12:39:40 -0400

    10x speedup on using pypy... jeez...

--- prob214.py
+++ prob214.py
@@ -6,11 +6,11 @@ prime_set = set()
 
 totient_val = [x for x in range(SIZE+1)]
 for i in xrange(2,SIZE):
-    if totient_val[i] == i:
-        for j in xrange(i,SIZE,i):
-            totient_val[j] = (i-1)*totient_val[j]/i
-        prime_set.add(i)
-    totient_val[i] = int(totient_val[i])
+  if totient_val[i] == i:
+    for j in xrange(i,SIZE,i):
+      totient_val[j] = (i-1)*totient_val[j]/i
+    prime_set.add(i)
+  totient_val[i] = int(totient_val[i])
 
 
 totient_count = [0] * (SIZE+1)
@@ -18,16 +18,15 @@ totient_count = [0] * (SIZE+1)
 print "Time Taken:", time.time() - START
 
 for i in xrange(2,SIZE):
-    totient_count[i] = totient_count[totient_val[i]] +1
+  totient_count[i] = totient_count[totient_val[i]] + 1
 
 
 print "Time Taken:", time.time() - START
 sumz = 0
 for i in prime_set:
-    if totient_count[i] == 24: sumz += i
+  if totient_count[i] == 24: sumz += i
 
 print sumz
-
 print "Time Taken:", time.time() - START
 
 
@@ -37,5 +36,15 @@ Time Taken: 74.8065619469
 1677366278943
 Time Taken: 75.3312809467
 ...I am not proud of this code... T_T
+
+
+
+# using pypy on palantir mac
+jchen@jchen-mbp 12:35:41 ~/Developer/proj_euler(master) % pypy prob214.py
+Time Taken: 7.4391720295
+Time Taken: 7.92977118492
+1677366278943
+Time Taken: 7.97728896141
+
 """
 
