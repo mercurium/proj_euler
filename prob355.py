b7fe9c9b0cc6062da506af5630723c7ca0c01827
Jerry Chen <jerrychen434@gmail.com>
2016-06-22 13:38:08 +0800

    solved problem 355

--- prob355.py
+++ prob355.py
@@ -1,5 +1,5 @@
 import time, math
-from primes import get_primes
+from primes import get_primes, factor
 
 START = time.time()
 SIZE  = 200000
@@ -20,40 +20,38 @@ for p in primes:
 forSureNum = set([1])
 unusedNum  = set()
 
+# filter out large numbers that can't be part of a product pq
 for index in xrange(len(primes)):
   prime = primes[index]
   if prime > SIZE / 2:
     forSureNum.add(prime)
     continue
-  prime_mult = SIZE / prime
 
-  if primes[index+1] < SIZE / prime_mult:
+  if primes[index+1] < SIZE / (SIZE // prime):
     forSureNum.add(max_prime_pow_dict[prime])
     continue
 
   unusedNum.add(prime)
 
-print len(unusedNum)
 print sum(forSureNum), len(forSureNum), len(unusedNum)
+print "\n"
 
-print "\n\n"
-
+merged = set()
 while len(unusedNum) != 0:
   print len(unusedNum)
   gainsDict = dict()
   for prime in unusedNum:
     insertMaxIntoDict(gainsDict, prime, (-1, prime, max_prime_pow_dict[prime], 0))
     for otherPrime in unusedNum:
-      for otherPrimePow in xrange(1, int(math.log(SIZE, otherPrime)+1)):
-        maxPow = int(math.log(SIZE/otherPrime**otherPrimePow, prime))
-        prod   = prime ** maxPow * otherPrime ** otherPrimePow
-        if prod > SIZE:
-          break
-        gain   = prod - max_prime_pow_dict[otherPrime] - max_prime_pow_dict[prime]
+      maxPow =  int(math.log(SIZE*1.0/otherPrime, prime))
+      prod   = prime ** maxPow * otherPrime
+      if prod > SIZE:
+        break
+      gain   = prod - max_prime_pow_dict[otherPrime] - max_prime_pow_dict[prime]
 
-        insertMaxIntoDict(gainsDict, prime, (gain, otherPrime, maxPow, otherPrimePow))
-        insertMaxIntoDict(gainsDict, otherPrime, (gain, prime, otherPrimePow, maxPow))
-
+      insertMaxIntoDict(gainsDict, prime, (gain, otherPrime, maxPow, 1))
+      insertMaxIntoDict(gainsDict, otherPrime, (gain, prime, 1, maxPow))
+  
   for n in gainsDict.keys():
     otherNum = gainsDict[n][1]
     if n not in unusedNum or otherNum not in unusedNum:
@@ -65,17 +63,38 @@ while len(unusedNum) != 0:
       continue
 
     if gainsDict[otherNum][1] == n:
-      forSureNum.add( n**gainsDict[n][2] * otherNum ** gainsDict[otherNum][2])
-      #print "\t", n, "\t", otherNum, "\t", gainsDict[n][0]
-
-      if n < SIZE**.5 and otherNum < SIZE**.5:
-        print n, otherNum
-
+      merged.add( n**gainsDict[n][2] * otherNum ** gainsDict[otherNum][2])
       unusedNum.remove(n)
       unusedNum.remove(otherNum)
 
-print sum(forSureNum), len(forSureNum)
+errors = True
+while errors:
+  errors = False
+  for m1 in sorted(merged):
+    if m1 not in merged:
+      continue
+    for m2 in sorted(merged):
+      if m2 not in merged or m1 not in merged:
+        continue
 
+      f1 = factor(m1)
+      f2 = factor(m2)
+      m11, m12 = f1[0], f1[-1]
+      m21, m22 = f2[0], f2[-1]
+      m21Pow =  int(math.log(SIZE*1.0/m12, m21))
+      m11Pow =  int(math.log(SIZE*1.0/m22, m11))
+  
+      if m1 + m2 < m11**m11Pow * m22 + m21 ** m21Pow * m12:
+        errors = True
+        merged.remove(m1)
+        merged.remove(m2)
+        merged.add(m11**m11Pow * m22)
+        merged.add(m21**m21Pow * m12)
+        print "Fixed:", m1, m2, m11, m12, m21, m22
+
+
+print sum(forSureNum) + sum(merged)
+print sum(forSureNum) + sum(merged) - 1726544591
 print "Time taken:", time.time() - START
 
 
@@ -84,6 +103,15 @@ print "Time taken:", time.time() - START
 Co(10) = {1,5,7,8,9}
 Co(30) = {1,11,13,17,19,23,25,27,28,29}
 CO(100) = {1, 17, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 81, 83, 88, 89, 91, 95, 97}
+
+
+
+Congratulations, the answer you gave to problem 355 is correct.
+
+You are the 354th person to have solved this problem.
+
+Answer: 1726545007
+Time taken: 0.53054189682
 
 
 """
