270e97cad9ee3e71805cd26ffb7d32220be1120e
Jerry Chen <jchen@palantir.com>
2016-09-23 12:42:52 -0400

    solved problem 446

--- prob446.py
+++ prob446.py
@@ -1,86 +1,119 @@
 #NOTE TODO need to solve it
 import time
-from primes import m_r
+from primes import m_r, gcd, primes, pollard_rho
+from heapq import *
 START = time.time()
-
+SIZE  = 10**5 * 2
+MOD   = 10**9 +7
 count = 0
-SIZE = 1024
-mod = 10**19 +7
 
+def isSquare(n):
+  a = int(n**.5+.5)
+  if a**2 == n:
+    return true
 
-def gcd(a,b):
-  while a != 0:
-    a,b = b%a, a
-  return b
+small_primes = primes[:1000]
 
-def pollard_rho(n):
-  def func(x):
-    return (x**2+1)%n
-  def func2(x):
-    return (x**2+3)%n
-  x,y = 2,2
-  d = 1
-  while d==1:
-    x = func(x)
-    y = func(func(y))
-    d = gcd(abs(x-y),n)
-
-  if d==n:
-    d=1
-    while d==1:
-      x = func2(x)
-      y = func2(func2(y))
-      d = gcd(abs(x-y),n)
-  return d
-
-small_primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,113,127,131,137,139,149,151,157,163,167,173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541]
-
+def insertIntoDict(valDict, key, value):
+    if key in valDict:
+      valDict[key] += value
+    else:
+      valDict[key] = value
 
 def factor(n):
   if m_r(n):
     return [n]
-  factor_lst = []
-  for i in small_primes:
-    while n % i == 0:
-      factor_lst.append(i)
-      n /= i
+  factorList = []
+  for p in small_primes:
+    while n % p == 0 and n > 1:
+      factorList.append(p)
+      n /= p
   while n != 1:
     if m_r(n):
-      return (factor_lst + [n])
+      return (factorList + [n])
     d = pollard_rho(n)
     while n % d == 0:
-      factor_lst.append(d)
+      factorList.append(d)
       n /= d
-  return factor_lst
+  return factorList
 
+heapA = []
+heappush(heapA, (10**9,10**9))
+heapB = []
+heappush(heapB, (10**9,10**9))
 
 for n in xrange(1,SIZE+1):
-  val_dict = dict()
-  a, b = n**2 - 2*n +2, n*n +2*n+2
-  factors = factor(a) + factor(b)
-  for p in factors:
-    if p in val_dict:
-      val_dict[p] +=1
-    else:
-      val_dict[p] = 1
-  prod = 1
-  for f in val_dict.keys():
-    prod = ((1 + pow(f, val_dict[f],mod)) * prod) % mod
-  count += prod - (n**4+4)% mod
+  valDict  = dict()
+
+  a        = n**2 - 2*n + 2
+  factorsA = []
+  while heapA[0][0] == n:
+    factorsA.append(heappop(heapA)[1])
+
+  factorsA = list(set(factorsA))
+  for f in factorsA:
+    heappush(heapA, (n+f, f))
+    while a % f == 0:
+      a /= f
+      insertIntoDict(valDict, f, 1)
+  factorsA = factor(a)
+  for f in set(factorsA):
+    heappush(heapA, (n+f, f))
+  for f in factorsA:
+    insertIntoDict(valDict, f, 1)
+
+
+  b        = n**2 + 2*n + 2
+  factorsB = []
+  while heapB[0][0] == n:
+    factorsB.append(heappop(heapB)[1])
+
+  factorsB = list(set(factorsB))
+  for f in factorsB:
+    heappush(heapB, (n+f, f))
+    while b % f == 0:
+      b /= f
+      insertIntoDict(valDict, f, 1)
+  factorsB = factor(b)
+  for f in set(factorsB):
+    heappush(heapB, (n+f, f))
+  for f in factorsB:
+    insertIntoDict(valDict, f, 1)
+
+  prod     = 1
+
+  for f in valDict.keys():
+    prod = ((1 + pow(f, valDict[f],MOD)) * prod) % MOD
+
+  count += prod - (n**4 + 4)% MOD
+
   if n% 1024 == 0:
     print n, time.time() - START
 
-print count %mod
+print count % MOD
 print "Time Taken:", time.time() - START
 
 
 """
 So, 2/20/2015, I'm looking at the code again. It seems to work. but I don't remember why. I didn't submit the problem, so I suspect that the limiting factor on this was the runtime. Yeah, for the 10**5 size problem, it's taken 18 seconds, and it only seems to be getting slower.
 
+Congratulations, the answer you gave to problem 446 is correct.
+
+You are the 206th person to have solved this problem.
+
+Answer: 907803852
+Time Taken: 747.287427902
+
+9/23/2016 notes:
+Lol, remembered that
+  n^4 + 4 = 0 mod p
+    then we have
+  (n+p)^4 + 4 = 0 mod p
+
+
 
 Size: 161792 Time Taken: 42.5061249733
 
 Definitely not going to scale, but let's figure out why it works first...
-
 
 """
