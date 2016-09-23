b0159d2cadaa15c40982cb4632cc2858d58c7386
Jerry Chen <jchen@palantir.com>
2016-09-23 13:23:12 -0400

    cleaned up problem 446

--- prob446.py
+++ prob446.py
@@ -3,14 +3,9 @@ import time
 from primes import m_r, gcd, primes, pollard_rho
 from heapq import *
 START = time.time()
-SIZE  = 10**5 * 2
+SIZE  = 10**7
 MOD   = 10**9 +7
 count = 0
-
-def isSquare(n):
-  a = int(n**.5+.5)
-  if a**2 == n:
-    return true
 
 small_primes = primes[:1000]
 
@@ -24,10 +19,6 @@ def factor(n):
   if m_r(n):
     return [n]
   factorList = []
-  for p in small_primes:
-    while n % p == 0 and n > 1:
-      factorList.append(p)
-      n /= p
   while n != 1:
     if m_r(n):
       return (factorList + [n])
@@ -37,58 +28,42 @@ def factor(n):
       n /= d
   return factorList
 
-heapA = []
-heappush(heapA, (10**9,10**9))
-heapB = []
-heappush(heapB, (10**9,10**9))
+heaps = [ [], [] ]
+heappush(heaps[0], (10**9,10**9)) # hack to get around dealing with empty heaps
+heappush(heaps[1], (10**9,10**9))
 
 for n in xrange(1,SIZE+1):
-  valDict  = dict()
+  # n^4 + 4 = (n^2 - 2n + 2)(n^2 + 2n + 2)
+  nFactors  = [n**2 - 2*n + 2, n**2 + 2*n + 2]
+  valDict   = dict()
 
-  a        = n**2 - 2*n + 2
-  factorsA = []
-  while heapA[0][0] == n:
-    factorsA.append(heappop(heapA)[1])
+  for i in xrange(2):
+    num     = nFactors[i]
+    factors = []
+    while heaps[i][0][0] == n:
+      factors.append(heappop(heaps[i])[1])
 
-  factorsA = list(set(factorsA))
-  for f in factorsA:
-    heappush(heapA, (n+f, f))
-    while a % f == 0:
-      a /= f
+    for f in factors:
+      heappush(heaps[i], (n+f, f))
+      while num% f == 0:
+        num /= f
+        insertIntoDict(valDict, f, 1)
+
+    for f in factor(num):
+      heappush(heaps[i], (n+f, f))
       insertIntoDict(valDict, f, 1)
-  factorsA = factor(a)
-  for f in set(factorsA):
-    heappush(heapA, (n+f, f))
-  for f in factorsA:
-    insertIntoDict(valDict, f, 1)
 
-
-  b        = n**2 + 2*n + 2
-  factorsB = []
-  while heapB[0][0] == n:
-    factorsB.append(heappop(heapB)[1])
-
-  factorsB = list(set(factorsB))
-  for f in factorsB:
-    heappush(heapB, (n+f, f))
-    while b % f == 0:
-      b /= f
-      insertIntoDict(valDict, f, 1)
-  factorsB = factor(b)
-  for f in set(factorsB):
-    heappush(heapB, (n+f, f))
-  for f in factorsB:
-    insertIntoDict(valDict, f, 1)
-
-  prod     = 1
+  prod = 1
 
   for f in valDict.keys():
     prod = ((1 + pow(f, valDict[f],MOD)) * prod) % MOD
 
-  count += prod - (n**4 + 4)% MOD
+  count += prod
 
   if n% 1024 == 0:
     print n, time.time() - START
+
+count = count - (6 * SIZE**5 + 15 * SIZE**4 + 10 * SIZE**3 - SIZE)/30 - 4*SIZE
 
 print count % MOD
 print "Time Taken:", time.time() - START
@@ -103,6 +78,8 @@ You are the 206th person to have solved this problem.
 
 Answer: 907803852
 Time Taken: 747.287427902
+Time Taken: 641.248315811 # Cleaned up previous code a bit
+
 
 9/23/2016 notes:
 Lol, remembered that
