487f82060ba106f81e0f53f610e5b3fb74c81e43
Jerry Chen <jchen@palantir.com>
2016-10-08 18:52:10 -0700

    cleaned up problem 105

--- prob105.py
+++ prob105.py
@@ -1,49 +1,38 @@
-import time
-start = time.time()
-import string
+import time, string
+START = time.time()
 from itertools import combinations
 
-data = open('sets.txt', 'r')
-data = data.read()
+data    = open('sets.txt', 'r')
+data    = data.read()
 subsets = string.split(data.strip(), '\n')
 for i in xrange(len(subsets)):
-    subsets[i] = [int(x) for x in string.split(subsets[i],',')]
+  subsets[i] = [int(x) for x in string.split(subsets[i],',')]
 
-sub_index = 'abcdefghijklmno'
-
-print len(subsets)
-sumz = 0
-count = 0
-for index in range(len(subsets)):
-    setz = subsets[index]
+sumz  = 0
+for index in xrange(len(subsets)):
+  setz        = subsets[index]
+  subset_sums = set([0])
+  SET_SIZE    = len(setz)
+  works       = True
+  for i in xrange(1,SET_SIZE):
+    if not works:
+      break
+    prev_max    = max(subset_sums)
     subset_sums = set()
-    TEMP_SUM = sum(setz)
-    SET_SIZE = len(setz)
-    works = True
-    for i in xrange(1,SET_SIZE):
-        if not works:
-            break
-        temp_set = set()
-        for j in combinations(sub_index[:SET_SIZE],i):
-            k = sum( setz[(ord(l)-97)] for l in j)
-            if k in subset_sums or k in temp_set:
-                works = False
-                break
-            elif len(subset_sums) >= 1 and k < max(subset_sums):
-                works = False
-                break
-            else:
-                temp_set.add(k)
-        for val in temp_set:
-            subset_sums.add(val)    
-    if works:
-        sumz += TEMP_SUM
-        count += 1
-        print "This set works:", setz
-        print TEMP_SUM, index, count, sumz
+    for j in combinations(range(SET_SIZE),i):
+      k = sum( setz[l] for l in j)
+      if k in subset_sums or k <= prev_max:
+        works = False
+        break
+      else:
+        subset_sums.add(k)
+  if works:
+    sumz += sum(setz)
+    print "This set works:", setz
+    print sum(setz), index, sumz
 
-print sumz, count
-print "Time Taken:", time.time() - start
+print sumz
+print "Time Taken:", time.time() - START
 
 """
 Congratulations, the answer you gave to problem 105 is correct.
