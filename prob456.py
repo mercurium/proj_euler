75b1457d334ffb1a56c00f4eac6a08370596e9fa
Jerry Chen <jchen@palantir.com>
2016-10-20 12:03:55 -0700

    fixed problem 456

--- prob456.py
+++ prob456.py
@@ -1,72 +1,86 @@
 import time
-start = time.time()
+START = time.time()
+SIZE  = 2*10**6
 
-SIZE =  2*10**6
+# This method is buggy
 def bsearch(lst,num, start=0,end=-1):
-    if end == -1:
-        end = len(lst)
-    index = (start+end)/2
+  if lst[-1] < num or lst[0] > num:
+    return 0,0
 
+  if end == -1:
+    end = len(lst)
+  index = (start+end)/2
+
+  while True:
     if start == end-1:
-        return index+1,len(lst)-index-1
+      above = index + 1
+      below = len(lst) - above
+      return above, below
     if lst[index] > num:
-        return bsearch(lst,num,start,(start+end)/2)
+      end   = index
+      index = (start+end)/2
     elif lst[index] < num:
-        return bsearch(lst,num,(start+end)/2, end)
+      start = index
+      index = (start+end)/2
+    else:
+      break
 
-    start, end = index,index #looks slow/stupid, but realistically, there won't be that many points with same slope
-    while lst[start] == num:
-        start -=1
-    while end < len(lst) and lst[end] == num:
-        end +=1
-    return start+1,len(lst)-end
+  #looks slow/stupid, but realistically, there won't be that many points with same slope
+  start, end = index,index
+  while lst[start] == num:
+    start -= 1
+  while end < len(lst) and lst[end] == num:
+    end   += 1
+  return start + 1, len(lst)-end
 
 
 def div(point):
-    if point[1] == 0:
-        return float('inf')
-    return 1.0 * point[0]/point[1]
+  if point[1] == 0:
+    return float('inf')
+  return 1.0 * point[0]/point[1]
 
 
-points = []
-for i in xrange(1,SIZE+1):
-    points.append( (pow(1248,i,32323)-16161, pow(8421,i,30103)-15051))
+points = [(1248,8421)]
+for i in xrange(SIZE-1):
+  x = points[-1][0] * 1248 % 32323
+  y = points[-1][1] * 8421 % 30103
+  points.append((x,y))
+
+points = map( lambda (x,y): (x-16161,y-15051), points)
 
 lst1 = [] #points above the x axis
 lst2 = [] #points below the x axis
-for point in points:
-    if point[1] > 0:
-        lst1.append(point) 
-    elif point[1] < 0:
-        lst2.append(point)
-    elif point[0] > 0:
-        lst1.append(point)
-    else:
-        lst2.append(point)
 
-lst1Slopes = sorted([div(p) for p in lst1]) #sorted lists of the slopes from the points
+for point in points:
+  if point[1] > 0:
+    lst1.append(point)
+  elif point[1] < 0:
+    lst2.append(point)
+  elif point[0] > 0:
+    lst1.append(point)
+  else:
+    lst2.append(point)
+
+print "Time Taken:", time.time() - START
+
+#sorted lists of the slopes from the points
+lst1Slopes = sorted([div(p) for p in lst1])
 lst2Slopes = sorted([div(p) for p in lst2])
 
+print "Time Taken:", time.time() - START
 ### Bottleneck starts here, no need to worry about slower code above yet.... ###
 
 count = 0
-for i in xrange(len(lst1)):
-    point = lst1[i]
-    above,below = bsearch(lst2Slopes,div(point))
-    count += above * below    
-    if i%16384 == 0: #counter variable to see how it's doing
-        print i
+for point in lst1:
+    above,below  = bsearch(lst2Slopes,div(point))
+    count       += above * below
 
-
-for i in xrange(len(lst2)):
-    point = lst2[i]
-    above,below = bsearch(lst1Slopes,div(point))
-    count += above * below    
-    if i%16384 == 0: #counter variable to see how it's doing
-        print i
+for point in lst2:
+    above,below  = bsearch(lst1Slopes,div(point))
+    count       += above * below
 
 print count
-print "Time Taken:", time.time() - start
+print "Time Taken:", time.time() - START
 
 """
 
