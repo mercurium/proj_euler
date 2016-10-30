83df00cfa11211e6df282bd382cc97bba749a9b1
Jerry Chen <jchen@palantir.com>
2016-10-30 10:40:53 -0700

    cleaned problem 50

--- prob050.py
+++ prob050.py
@@ -1,26 +1,25 @@
-# NOTE this needs more refactoring too...
-
-from bitarray import bitarray
 from primes import m_r, primes
 import time
 START = time.time()
 
-maxz = 0
-for startingPrime in xrange(0,20):
-    lst = [0]*546
-    sumz = 0
-    for k in xrange(startingPrime,600):
-        if sumz+primes[k] < 10**6:
-            sumz += primes[k]
-            lst[k] = sumz
-        else:
-            break
+SIZE           = 10**6
+MAX_PRIMES_LIM = 600 # sum of [2,3,5,7, ...] first 546 primes = 997,661
 
+maxNumPrimes = 0
+for startingPrime in xrange(0, MAX_PRIMES_LIM):
+  if sum(primes[startingPrime:startingPrime + maxNumPrimes]) > SIZE:
+    break
 
-    for i in xrange(k-1,0,-1):
-            if m_r(lst[i]):
-                if maxz < i:
-                    print lst[i], i
-                    maxz = i
-                break
+  lst  = primes[:MAX_PRIMES_LIM]
+  k    = startingPrime + 1
+  while lst[k-1] < SIZE:
+    lst[k] += lst[k-1]
+    k      += 1
+
+  for i in xrange(k-1,0,-1):
+    if m_r(lst[i]):
+      if maxNumPrimes < i:
+        print lst[i], i
+        maxNumPrimes = i
+      break
 print "Time taken:",time.time() -START
