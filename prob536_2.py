ebdebf1bc47dbe9ee38d711dd5b00ef3f67dc25b
Jerry Chen <jerrychen434@gmail.com>
2016-12-19 00:54:06 +0800

    checkpoint prob536 again

--- prob536_2.py
+++ prob536_2.py
@@ -1,16 +1,19 @@
 import time, math
 from primes import get_primes
+from heapq import *
 
 START        = time.time()
-SIZE         = 10**6
+SIZE         = 10**8
 factorDict   = {1 : [], 2: [2]}
 listOfPrimes = get_primes(int(SIZE**.5))
 
 def factor(n):
   return factorDict[n]
 
-def checkPropertyLessStupid(m):
-  for f in factor(m):
+def checkPropertyLessStupid(m, factors=None):
+  if not factors:
+    factors = factor(m)
+  for f in factors:
     if (m / f + 3) % (f - 1) != 0:
       return False
   return True
@@ -18,32 +21,60 @@ cpls = checkPropertyLessStupid
 
 
 possibleAnswers = set([1])
+primesUsed = set()
+answers    = set()
+
+firstTime  = True
+
+#possibleAnswers = []
+#heapify(possibleAnswers)
+
 for prime in listOfPrimes[1:]:
+  print prime, len(possibleAnswers)
   newAnswers = set()
+
+  if prime**3 > SIZE:
+    for n in possibleAnswers:
+      if n > prime and n * prime < SIZE:
+        if cpls(n*prime, factorDict[n] + [prime]):
+          answers.add(n*prime)
+
+    # stupid minor optimization
+    if prime % 100 == 37:
+      for n in sorted(possibleAnswers):
+        if n*prime > SIZE:
+          if cpls(n):
+            answers.add(n)
+          possibleAnswers.remove(n)
+    continue
+
   for n in possibleAnswers:
     if n * prime < SIZE:
-      factorDict[n*prime] = factorDict[n] + [prime]
-      newAnswers.add(n*prime)
+      if n * prime**2 > SIZE:
+        if cpls(n*prime, factorDict[n] + [prime]):
+          answers.add(n*prime)
+          #primesUsed.update([prime] + factor(n))
+      else:
+        factorDict[n*prime] = factorDict[n] + [prime]
+        newAnswers.add(n*prime)
   possibleAnswers.update(newAnswers)
+
+
 
 possibleAnswers.add(2)
 possibleAnswers = sorted(possibleAnswers)
 print "Time Taken:", time.time() - START
 
-sumz  = 0
-count = 0
-
-primesUsed = set()
 for i in possibleAnswers:
   if cpls(i):
-    sumz  += i
-    count += 1
-    print i, factor(i)
+    answers.add(i)
+    #print i, factor(i)
     primesUsed.update(set(factor(i)))
 
-print sumz, count, len(possibleAnswers)
+
+print sum(answers), len(answers), len(possibleAnswers)
+#print sorted(answers)
 print sorted(primesUsed)
-print sorted(set(listOfPrimes).difference(primesUsed))
 
 print "Time Taken:", time.time() - START
 
@@ -62,11 +93,12 @@ print "Time Taken:", time.time() - START
     7((7-4) + 1(7-1)) = 21 + 42 = 63. <-- Doesn't work since 63 = 3^2 * 7, but it satisfies the 7 req
     7((7-4) + 2(7-1)) = 21 + 84 = 105. <--- actual answer that works
 
-  10^2 has 5   answers
-  10^3 has 9   answers
-  10^4 has 15  answers
-  10^5 has 36  answers
-  10^6 has 84  answers
-  10^7 has 175 answers
+  10^2 has 5    answers
+  10^3 has 9    answers
+  10^4 has 15   answers
+  10^5 has 36   answers
+  10^6 has 84   answers
+  10^7 has 175  answers
+  10^8 has 1038 answers
 
 """
